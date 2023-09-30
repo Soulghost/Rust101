5d6419e822ace40523f5c61755f6589c56971422
Soulghost <xiuyutong1994@163.com>
2023-09-30 23:10:37 +0800

    feat: add bvh intersection algorithm

--- path_tracing/src/bvh/bounds.rs
+++ path_tracing/src/bvh/bounds.rs
@@ -1,4 +1,6 @@
-use crate::{math::vector::Vector3f, domain::domain::Axis};
+use std::f32::EPSILON;
+
+use crate::{math::vector::Vector3f, domain::domain::{Axis, Ray}};
 pub struct Bounds3 {
     pub p_min: Vector3f,
     pub p_max: Vector3f
@@ -38,4 +40,36 @@ impl Bounds3 {
             p_max: Vector3f::max(&a.p_max, &b.p_max),
         }
     }
+
+    pub fn intersect(&self, ray: &Ray) -> bool {
+        let inv_dir = Vector3f::new(
+            1.0 / (ray.direction.x + EPSILON),
+            1.0 / (ray.direction.y + EPSILON),
+            1.0 / (ray.direction.z + EPSILON)
+        );
+        let is_dir_neg = vec![
+            ray.direction.x >= EPSILON,
+            ray.direction.y >= EPSILON,
+            ray.direction.z >= EPSILON
+        ];
+        let origin = &ray.origin;
+        let p_min = &self.p_min;
+        let p_max = &self.p_max;
+        let t_min = &(p_min - origin) * &inv_dir;
+        let t_max = &(p_max - origin) * &inv_dir;
+        let mut t_enter3 = Vector3f::zero();
+        let mut t_exit3 = Vector3f::zero();
+        t_enter3.x = if is_dir_neg[0] { t_min.x } else { t_max.x };
+        t_enter3.y = if is_dir_neg[0] { t_min.y } else { t_max.y };
+        t_enter3.z = if is_dir_neg[0] { t_min.z } else { t_max.z };
+
+        t_exit3.x = if !is_dir_neg[0] { t_min.x } else { t_max.x };
+        t_exit3.y = if !is_dir_neg[0] { t_min.y } else { t_max.y };
+        t_exit3.z = if !is_dir_neg[0] { t_min.z } else { t_max.z };
+
+        let t_enter = f32::max(t_enter3.x, f32::max(t_enter3.y, t_enter3.z));
+        let t_exit = f32::min(t_exit3.x, f32::min(t_exit3.y, t_exit3.z));
+        return t_exit >= t_enter && t_exit >= EPSILON;
+    }
+
 }
--- path_tracing/src/mesh/object.rs
+++ path_tracing/src/mesh/object.rs
@@ -1,4 +1,4 @@
-use crate::bvh::bounds::Bounds3;
+use crate::{bvh::bounds::Bounds3, domain::domain::{Intersection, Ray}};
 
 pub trait Object {
     fn get_bounds(&self) -> Bounds3 {
@@ -8,4 +8,6 @@ pub trait Object {
     fn get_area(&self) -> f32 {
         return 0.0;
     }
+
+    fn intersect(&self, ray: &Ray) -> Intersection;
 }
