64321be0c9292f6bd841aa0fab5d0cb2ba86f5e0
Soulghost <xiuyutong1994@163.com>
2023-10-14 20:02:12 +0800

    feat: format main.rs

--- path_tracing/src/main.rs
+++ path_tracing/src/main.rs
@@ -1,69 +1,79 @@
 extern crate lazy_static;
 
-use std::sync::Arc;
 use material::material::LitMaterial;
 use math::vector::Vector3f;
 use mesh::model::Model;
 use scene::Scene;
+use std::sync::Arc;
 
-use crate::renderer::{renderer::Renderer, framebuffer::FrameBuffer};
+use crate::renderer::{framebuffer::FrameBuffer, renderer::Renderer};
 
-pub mod scene;
-pub mod mesh;
 pub mod bvh;
-pub mod math;
 pub mod domain;
 pub mod material;
+pub mod math;
+pub mod mesh;
 pub mod renderer;
+pub mod scene;
 pub mod util;
 
 fn main() {
-    let width = 500; 
-    let height = 500; 
-    let spp = 128; 
+    let width = 500;
+    let height = 500;
+    let spp = 128;
     let n_threads = 12;
-    let mut scene = Scene::new(width, 
-                                      height, 
-                                 40.0, 
-             Vector3f::new(0.235294, 0.67451, 0.843137), 
-                    scene::EstimatorStrategy::RussianRoulette(0.8),
-                    spp);
-
-    let white_mat = Arc::new(
-        LitMaterial::new(&Vector3f::new(0.725, 0.71, 0.68), &Vector3f::zero())
-    );
-    let red_mat = Arc::new(
-        LitMaterial::new(&Vector3f::new(0.63, 0.065, 0.05), &Vector3f::zero())
-    );
-    let green_mat = Arc::new(
-        LitMaterial::new(&Vector3f::new(0.14, 0.45, 0.091), &Vector3f::zero())
+    let mut scene = Scene::new(
+        width,
+        height,
+        40.0,
+        Vector3f::new(0.235294, 0.67451, 0.843137),
+        scene::EstimatorStrategy::RussianRoulette(0.8),
+        spp,
     );
 
-    let light_emission_color = Vector3f::new(0.747 + 0.058, 0.747 + 0.258, 0.747) * 8.0 + Vector3f::new(0.740 + 0.287, 0.740 + 0.160, 0.740) * 15.6 + Vector3f::new(0.737 + 0.642, 0.737 + 0.159,0.737) * 18.4;
+    let white_mat = Arc::new(LitMaterial::new(
+        &Vector3f::new(0.725, 0.71, 0.68),
+        &Vector3f::zero(),
+    ));
+    let red_mat = Arc::new(LitMaterial::new(
+        &Vector3f::new(0.63, 0.065, 0.05),
+        &Vector3f::zero(),
+    ));
+    let green_mat = Arc::new(LitMaterial::new(
+        &Vector3f::new(0.14, 0.45, 0.091),
+        &Vector3f::zero(),
+    ));
+
+    let light_emission_color = Vector3f::new(0.747 + 0.058, 0.747 + 0.258, 0.747) * 8.0
+        + Vector3f::new(0.740 + 0.287, 0.740 + 0.160, 0.740) * 15.6
+        + Vector3f::new(0.737 + 0.642, 0.737 + 0.159, 0.737) * 18.4;
     let light_color = Vector3f::new(0.65, 0.65, 0.65);
-    let light_mat = Arc::new(
-        LitMaterial::new(&light_color,
-        &light_emission_color)
-    );
-    let floor = Arc::new(
-        Model::new("./resource/cornellbox/floor.obj", white_mat.clone())
-    );
-    let shortbox = Arc::new(
-        Model::new("./resource/cornellbox/shortbox.obj", white_mat.clone())
-    );
-    let tallbox = Arc::new(
-        Model::new("./resource/cornellbox/tallbox.obj", white_mat.clone())
-    );
-    let left = Arc::new(
-        Model::new("./resource/cornellbox/left.obj", red_mat.clone())
-    );
-    let right = Arc::new(
-        Model::new("./resource/cornellbox/right.obj", green_mat.clone())
-    );
-    let light = Arc::new(
-        Model::new("./resource/cornellbox/light.obj", light_mat.clone())
-    );
-      
+    let light_mat = Arc::new(LitMaterial::new(&light_color, &light_emission_color));
+    let floor = Arc::new(Model::new(
+        "./resource/cornellbox/floor.obj",
+        white_mat.clone(),
+    ));
+    let shortbox = Arc::new(Model::new(
+        "./resource/cornellbox/shortbox.obj",
+        white_mat.clone(),
+    ));
+    let tallbox = Arc::new(Model::new(
+        "./resource/cornellbox/tallbox.obj",
+        white_mat.clone(),
+    ));
+    let left = Arc::new(Model::new(
+        "./resource/cornellbox/left.obj",
+        red_mat.clone(),
+    ));
+    let right = Arc::new(Model::new(
+        "./resource/cornellbox/right.obj",
+        green_mat.clone(),
+    ));
+    let light = Arc::new(Model::new(
+        "./resource/cornellbox/light.obj",
+        light_mat.clone(),
+    ));
+
     scene.add(floor);
     scene.add(shortbox);
     scene.add(tallbox);
@@ -78,9 +88,11 @@ fn main() {
     renderer.fbo = Some(fbo);
 
     println!("[Main] start rendering...");
-    renderer.render(final_scene, n_threads).unwrap_or_else(|err| {
-        panic!("[Main] renderer error {}", err);
-    });
+    renderer
+        .render(final_scene, n_threads)
+        .unwrap_or_else(|err| {
+            panic!("[Main] renderer error {}", err);
+        });
     println!("[Main] end rendering...");
 
     let fbo = renderer.fbo.as_mut().unwrap();
